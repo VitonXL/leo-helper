fedc84fef134cb8baca0a7035d1cf114ba947eca
VitonXL <Saha300351@mail.ru>
2025-12-12 13:06:33 +0300

    Update worker.py

--- worker.py
+++ worker.py
@@ -1,6 +1,18 @@
 # worker.py
 
-from bot.bot import bot_main
+import os
+import threading
+from datetime import time
+from bot.bot import application
+from web.app import app as web_app
+
+def run_web():
+    port = int(os.getenv("PORT", 8000))
+    web_app.run(host="0.0.0.0", port=port)
 
 if __name__ == "__main__":
-    bot_main()
+    # Запускаем веб в фоновом потоке
+    threading.Thread(target=run_web, daemon=True).start()
+
+    # Запускаем бота
+    application.run_polling()
