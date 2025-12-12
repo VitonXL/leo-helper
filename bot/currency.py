d5e635cd52dc7391821af9516aec89fbda9f849b
VitonXL <Saha300351@mail.ru>
2025-12-12 09:42:26 +0300

    Delete bot directory

--- bot/currency.py
+++
@@ -1,9 +0,0 @@
-# bot/currency.py
-import requests
-
-def get_usd_rate():
-    try:
-        r = requests.get("https://api.exchangerate-api.com/v4/latest/USD").json()
-        return round(r["rates"]["RUB"], 2)
-    except:
-        return "91.20"
--- bot/worker.py
+++
@@ -1,7 +0,0 @@
-# worker.py
-
-# Импортируем и запускаем бота
-from bot.bot import bot_main
-
-if __name__ == "__main__":
-    bot_main()
